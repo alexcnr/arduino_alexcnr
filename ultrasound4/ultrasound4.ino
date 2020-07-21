553d66bfbf7eb7f597acaf0370a6c24d302acac7
Zharov Alexander <54246135+alexcnr@users.noreply.github.com>
2020-07-21 08:25:51 +0300

    Парктроник
    Загорается светодиод при приближении к УЗ-датчику

--- ultrasound4/ultrasound4.ino
+++ ultrasound4/ultrasound4.ino
@@ -2,14 +2,13 @@
 #include "Segment.h"
 #include "Digit.h"
 #include "Display.h"
-
 #include <Arduino.h>
-
-//#define pinIn  A0
  
 #define TRIGGER_PIN  2
 #define ECHO_PIN     3
 #define MAX_DISTANCE 400
+
+#define flash_pin 13
 
 const byte digit_pins[4] {A1, A2, A3, A4}; 
 const byte segment_pins[7] {5, 6, 7, 8, 9, 10, 11}; 
@@ -27,20 +26,32 @@ void setup() {
   for (int i = 0; i < 8; i++) {
     pinMode(segment_pins[i], OUTPUT);
   }
+  pinMode(dp_pin, OUTPUT);
+  pinMode(flash_pin, OUTPUT);
+  digitalWrite(flash_pin, LOW);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
  
 void loop() {
     delay(5);
-  Serial.print("Distance: ");
-//  Serial.print(sonar.ping_cm());
+    
+ // Serial.print("Distance: ");
+  Serial.print(sonar.ping_cm());
  float distance = sonar.ping_cm();
  // Serial.println(sonar.ping_cm());
-//  Serial.print("int distance: "); Serial.println(distance);
+ // Serial.print("int distance: "); Serial.println(distance);
 char valueChars[4];
   dtostrf(distance, 3, 3, valueChars);
-  Serial.print("valueChars: "); Serial.println(valueChars);
-  delay(5);
-  d.print(valueChars); 
+ // Serial.print("valueChars: "); 
+ // Serial.println(valueChars);
+  delay(1);
+  d.print(valueChars);
+  if(distance < 10){
+    digitalWrite(flash_pin, HIGH);
+        } 
+  else {
+      digitalWrite(flash_pin, LOW);
+    
+  }
 }
