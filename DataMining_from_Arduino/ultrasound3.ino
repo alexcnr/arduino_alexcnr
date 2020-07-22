ccc1f99cac9807bbc73b1d1801f18f7cefe3ad02
Zharov Alexander <54246135+alexcnr@users.noreply.github.com>
2020-07-22 09:42:57 +0300

    Принимаем поток с Arduino
    Результаты в список, а также в csv файл

---
+++ DataMining_from_Arduino/ultrasound3.ino
@@ -0,0 +1,20 @@
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  2
+#define ECHO_PIN     3
+#define MAX_DISTANCE 400
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  delay(50);
+ // Serial.print("Distance: ");
+//  Serial.print(sonar.ping_cm().encode('utf-8'));
+ int distance = sonar.ping_cm();
+ Serial.println(sonar.ping_cm());
+  
+}
