592f17a7d7da6d4415d4647cc8e39d48d59f1ace
Zharov Alexander <54246135+alexcnr@users.noreply.github.com>
2020-07-23 11:52:26 +0300

    Данные с термодатчика и в SQLlite
    4 показателя

---
+++ termo_v1.0/work.py
@@ -0,0 +1,9 @@
+#Преобразование string во float в списке
+str_lst = ['10.505', '2.3', '3.99']
+float_lst = [float(x) for x in str_lst]
+print (float_lst[0])
+
+#Пример преобразования string в int в списке
+str_lst = ['1', '2', '3']
+int_lst = [int(x) for x in str_lst]
+print (int_lst)
